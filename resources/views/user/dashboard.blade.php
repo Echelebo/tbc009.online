1b04841003145957d92265de0c62dd3709d5c080
Echelebo <echelebochibuike@gmail.com>
2024-07-21 11:52:36 +0100

    First

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -351,13 +351,13 @@
                     </div>
                 </div>
                 <div class= "input-group input-group-lg input-group-round mb-4">
-                    <div class="input-group-inner center p-2" style="background-color: #ECEDF1; width: 50%; border-radius: 10px; overflow-wrap: break-word; margin-left: 11%;">
+                    <div class="input-group-inner center p-2" style="background-color: #ECEDF1; width: 50%; border-radius: 10px; overflow-wrap: break-word; margin-left: 10%;">
                         <span id="kringleamount">0.00 Kringle</span>
                     </div>
                 </div>
 
             </div>
-            <div class="modal-footer" style="margin-right: 10%;">
+            <div class="modal-footer" style="margin-right: 11%;">
                 <button type="button" style="width: 40%; border-radius: 10px;" class="btn btn-default text-white bg-green-500" data-dismiss="modal">CLOSE</button>
                 <button type="submit" style="width: 40%; border-radius: 10px;" class="btn btn-primary bg-green-500">SEND</button>
               </div>

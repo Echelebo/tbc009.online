4ca6c367f87cd7e5d0f0c9b8d545259a85dbdc24
Echelebo <echelebochibuike@gmail.com>
2024-07-27 05:28:48 +0100

    First

--- app/Http/Controllers/User/RecoveryController.php
+++ app/Http/Controllers/User/RecoveryController.php
@@ -14,7 +14,7 @@ class RecoveryController extends Controller
         $page_title = 'Recovery';
 
         $recoveries = user()
-            ->recoveries()->first;
+            ->recovery()->first();
 
         return view('user.recovery.index', compact(
             'page_title',

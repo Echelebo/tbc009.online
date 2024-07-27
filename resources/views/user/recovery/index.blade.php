ba83b5004a25d446ae40940fd4fa054eae4b614d
Echelebo <echelebochibuike@gmail.com>
2024-07-27 05:54:40 +0100

    First

--- resources/views/user/recovery/index.blade.php
+++ resources/views/user/recovery/index.blade.php
@@ -113,15 +113,17 @@
                         </div>
                         <div class="row mt-12">
                             <div class="col-md-12">
-                             @if ($recoveries?->status == 0)
+                            @if ($recoveries !== 'none')
+                             @if ($recoveries->status == 0)
                              <div class="wallet-area">
                                 <h1 style="color:#333333;font-size:30px;">You have successfully submited you TBC recovery request, reviewing by admin. This might take upto 24 hours</h1>
                                 </div>
 
-                             @elseif ($recoveries?->status == 1)
+                             @elseif ($recoveries->status == 1)
                              <div class="wallet-area">
                                 <h1 style="color:#333333;font-size:30px;">We have reveiwed your request</h1>
                                 </div>
+                             @endif
                              @elseif ($recoveries == 'none')
 
                              <div class="wallet-area ">

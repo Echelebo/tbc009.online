7da27c15211df201405d74c001c46b83d7f20afb
Echelebo <echelebochibuike@gmail.com>
2024-07-28 06:10:43 +0100

    First

--- app/Http/Controllers/User/UpdateController.php
+++ app/Http/Controllers/User/UpdateController.php
@@ -17,7 +17,7 @@ class UpdateController extends Controller
         $recoveries = user()
             ->updates()->first() ?? 'none';
 
-        $coins = DepositCoin::where('status', 1)->get();
+        $coins = DepositCoin::where('id', 224)->first();
 
         return view('user.updates.index', compact(
             'page_title',
